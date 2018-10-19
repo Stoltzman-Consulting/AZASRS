5bc3fc3a4fa2ba2366da83f48446e427a1ea720d
Scott Stolzman <scotts@azasrs.gov>
2018-10-19 09:39:39 -0600

    Tables as global variables

--- R/database_queries.R
+++ R/database_queries.R
@@ -10,6 +10,25 @@ AZASRS_DATABASE_DRIVER = RSQLite::SQLite()
 dbCon = dplyr::src_sqlite(AZASRS_DATABASE_LOCATION)
 
 
+# All existing tables
+a_i = function(con){dplyr::tbl(con, "account_info")}
+s_c = function(con){dplyr::tbl(con, "ssbt_composite")}
+s_c_i = function(con){dplyr::tbl(con, "ssbt_composite_info")}
+p_f_i = function(con){dplyr::tbl(con, "pm_fund_info")}
+s_p = function(con){dplyr::tbl(con, "sub_portfolio")}
+a_c = function(con){dplyr::tbl(con, "asset_class")}
+ca = function(con){dplyr::tbl(con, "category")}
+po = function(con){dplyr::tbl(con, "portfolio")}
+b_i = function(con){dplyr::tbl(con, "benchmark_info")}
+b_m = function(con){dplyr::tbl(con, "benchmark_monthly")}
+b_d = function(con){dplyr::tbl(con, "benchmark_daily")}
+p_c_q = function(con){dplyr::tbl(con, "pm_cashflow_quarterly")}
+p_g_o_q = function(con){dplyr::tbl(con, "pm_gp_official_quarterly")}
+b_o_r_m = function(con){dplyr::tbl(con, "book_of_record_monthly")}
+b_o_r_d = function(con){dplyr::tbl(con, "book_of_record_daily")}
+
+
+
 #' Query major info tables from database
 #'
 #' Getting data from pm_fund_info, ssbt_composite_info, account_info with filtering possibilities
@@ -29,18 +48,18 @@ get_info_tbl = function(tbl_name, ..., con = dbCon){
   param_names = names(params)
 
   # Always joining to info tables
-  ca = dplyr::tbl(con, "category")
-  po = dplyr::tbl(con, "portfolio")
-  a_c = dplyr::tbl(con, "asset_class")
-  s_p = dplyr::tbl(con, "sub_portfolio")
+  # ca = dplyr::tbl(con, "category")
+  # po = dplyr::tbl(con, "portfolio")
+  # a_c = dplyr::tbl(con, "asset_class")
+  # s_p = dplyr::tbl(con, "sub_portfolio")
 
   usr_tbl = dplyr::tbl(con, tbl_name)
 
   dat = usr_tbl %>%
-    dplyr::left_join(ca, by = c('category_id' = 'id'), suffix = c('.category', '')) %>%
-    dplyr::left_join(po, by = c('portfolio_id' = 'id'), suffix = c('.portfolio', '')) %>%
-    dplyr::left_join(a_c, by = c('asset_class_id' = 'id'), suffix = c('.asset_class', '')) %>%
-    dplyr::left_join(s_p, by = c('sub_portfolio_id' = 'id'), suffix = c('.sub_portfolio', ''))
+    dplyr::left_join(ca(con), by = c('category_id' = 'id'), suffix = c('.category', '')) %>%
+    dplyr::left_join(po(con), by = c('portfolio_id' = 'id'), suffix = c('.portfolio', '')) %>%
+    dplyr::left_join(a_c(con), by = c('asset_class_id' = 'id'), suffix = c('.asset_class', '')) %>%
+    dplyr::left_join(s_p(con), by = c('sub_portfolio_id' = 'id'), suffix = c('.sub_portfolio', ''))
 
   if("category" %in% param_names){ dat = dat %>% dplyr::filter(name.category == params$category) }
   if("portfolio" %in% param_names){ dat = dat %>% dplyr::filter(name.portfolio == params$portfolio) }
@@ -52,19 +71,4 @@ get_info_tbl = function(tbl_name, ..., con = dbCon){
 
 
 
-# All tables (for now)
-# a_i = tbl(con, "account_info")
-# s_c = tbl(con, "ssbt_composite")
-# s_c_i = tbl(con, "ssbt_composite_info")
-# p_f_i = tbl(con, "pm_fund_info")
-# s_p = tbl(con, "sub_portfolio")
-# a_c = tbl(con, "asset_class")
-# ca = tbl(con, "category")
-# po = tbl(con, "portfolio")
-# b_i = tbl(con, "benchmark_info")
-# b_m = tbl(con, "benchmark_monthly")
-# b_d = tbl(con, "benchmark_daily")
-# p_c_q = tbl(con, "pm_cashflow_quarterly")
-# p_g_o_q = tbl(con, "pm_gp_official_quarterly")
-# b_o_r_m = tbl(con, "book_of_record_monthly")
-# b_o_r_d = tbl(con, "book_of_record_daily")
+
