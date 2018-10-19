c36cea348acd38484803ea2eca99f54974fd159a
Scott Stolzman <scotts@azasrs.gov>
2018-10-19 17:04:56 -0600

    Clean up code

--- R/get_info_tbl.R
+++ R/get_info_tbl.R
@@ -3,11 +3,11 @@
 #'
 #' @param tbl_name is the name of the major info table (choose one): pm_fund_info, ssbt_composite_info, account_info
 #' @param ... are for filtering the tables, can take values: category, portfolio, asset_class, sub_portfolio
-#' @return A table object from dplyr
+#' @return Returns a tibble of all of the data
 #' @examples
-#' get_info_tbl('pm_fund_info', sub_portfolio='Equity')
+#' get_tbl_info('pm_fund_info', sub_portfolio = 'Equity')
 #' @export
-get_info_tbl = function(tbl_name, ..., con = AZASRS_DATABASE_CONNECTION()){
+get_tbl_info = function(tbl_name, ..., con = AZASRS_DATABASE_CONNECTION()){
 
   if(!(tbl_name %in% c("pm_fund_info", "ssbt_composite_info", "account_info"))){
     return("Table name must be one the following: 'pm_fund_info', 'ssbt_composite_info', 'account_info'")
@@ -25,15 +25,18 @@ get_info_tbl = function(tbl_name, ..., con = AZASRS_DATABASE_CONNECTION()){
   usr_tbl = dplyr::tbl(con, tbl_name)
 
   dat = usr_tbl %>%
-    dplyr::left_join(category(con), by = c('category_id' = 'id'), suffix = c('.category', '')) %>%
-    dplyr::left_join(portfolio(con), by = c('portfolio_id' = 'id'), suffix = c('.portfolio', '')) %>%
-    dplyr::left_join(asset_class(con), by = c('asset_class_id' = 'id'), suffix = c('.asset_class', '')) %>%
-    dplyr::left_join(sub_portfolio(con), by = c('sub_portfolio_id' = 'id'), suffix = c('.sub_portfolio', ''))
+    dplyr::left_join(tbl_category(con), by = c('category_id' = 'id')) %>% mutate(name.category = name) %>%
+    dplyr::left_join(tbl_portfolio(con), by = c('portfolio_id' = 'id')) %>% mutate(name.portfolio = name.y) %>%
+    dplyr::left_join(tbl_asset_class(con), by = c('asset_class_id' = 'id')) %>% mutate(name.asset_class = name) %>%
+    dplyr::left_join(tbl_sub_portfolio(con), by = c('sub_portfolio_id' = 'id')) %>% mutate(name.sub_portfolio = name.y.y)
 
   if("category" %in% param_names){ dat = dat %>% dplyr::filter(name.category == params$category) }
   if("portfolio" %in% param_names){ dat = dat %>% dplyr::filter(name.portfolio == params$portfolio) }
   if("asset_class" %in% param_names){ dat = dat %>% dplyr::filter(name.asset_class == params$asset_class) }
   if("sub_portfolio" %in% param_names){ dat = dat %>% dplyr::filter(name.sub_portfolio == params$sub_portfolio) }
 
-  return(dat)
+  dat = dat %>%
+    select(ssbt_id, description, name.category, name.portfolio, name.asset_class, name.sub_portfolio)
+
+  return(dat %>% tibble::as_tibble())
 }
