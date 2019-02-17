4797b3d79768b62263cb0b8d786427ffaf41dc01
Scott Stolzman <scotts@azasrs.gov>
2019-02-17 14:53:00 -0700

    new tables added

--- R/database_globals.R
+++ R/database_globals.R
@@ -1,8 +1,8 @@
 
 # Global variables
 #' @export
-#AZASRS_DATABASE_LOCATION = "C:\\Users\\scotts\\Documents\\GitHub\\ASRS-Application\\database\\asrs.db"
-AZASRS_DATABASE_LOCATION = "P:\\IMD\\2018 Database Project\\asrs_new.db"
+AZASRS_DATABASE_LOCATION = "C:\\Users\\scotts\\Desktop\\2018 Database Project\\Database\\asrs_database.db"
+#AZASRS_DATABASE_LOCATION = "P:\\IMD\\2018 Database Project\\asrs_new.db"
 
 #' @export
 AZASRS_DATABASE_DRIVER = RSQLite::SQLite()
@@ -10,29 +10,43 @@ AZASRS_DATABASE_DRIVER = RSQLite::SQLite()
 #' @export
 AZASRS_DATABASE_CONNECTION = function(){ return(dplyr::src_sqlite(AZASRS_DATABASE_LOCATION)) }
 
-
 # All existing tables
 
 #' @export
-tbl_account_info = function(con){dplyr::tbl(con, "account_info")}
-
-#' @export
-tbl_ssbt_composite = function(con){dplyr::tbl(con, "ssbt_composite")}
-
-#' @export
-tbl_ssbt_composite_info = function(con){dplyr::tbl(con, "ssbt_composite_info")}
-
-#' @export
-tbl_pm_fund_info = function(con){dplyr::tbl(con, "pm_fund_info")}
-
-#' @export
-tbl_sub_portfolio = function(con){dplyr::tbl(con, "sub_portfolio")}
+tbl_account_info = function(con){df = dplyr::tbl(con, "account_info")}
 
 #' @export
 tbl_asset_class = function(con){dplyr::tbl(con, "asset_class")}
 
 #' @export
+tbl_benchmark_info = function(con){dplyr::tbl(con, "benchmark_info")}
+
+#' @export
+tbl_book_of_record_account_daily = function(con){dplyr::tbl(con, "book_of_record_account_daily")}
+
+#' @export
+tbl_book_of_record_account_monthly = function(con){dplyr::tbl(con, "book_of_record_account_monthly")}
+
+#' @export
+tbl_book_of_record_composite_daily = function(con){dplyr::tbl(con, "book_of_record_composite_daily")}
+
+#' @export
+tbl_book_of_record_composite_monthly = function(con){dplyr::tbl(con, "book_of_record_composite_monthly")}
+
+#' @export
 tbl_category = function(con){dplyr::tbl(con, "category")}
+
+#' @export
+tbl_city = function(con){dplyr::tbl(con, "city")}
+
+#' @export
+tbl_pm_cashflow_daily = function(con){dplyr::tbl(con, "pm_cashflow_daily")}
+
+#' @export
+tbl_pm_fund_info = function(con){dplyr::tbl(con, "pm_fund_info")}
+
+#' @export
+tbl_pm_nav_daily = function(con){dplyr::tbl(con, "pm_nav_daily")}
 
 #' @export
 tbl_portfolio = function(con){dplyr::tbl(con, "portfolio")}
@@ -44,34 +58,10 @@ tbl_previous_saa = function(con){dplyr::tbl(con, "previous_saa")}
 tbl_sponsor = function(con){dplyr::tbl(con, "sponsor")}
 
 #' @export
-tbl_saa_benchmark = function(con){dplyr::tbl(con, "saa_benchmark")}
+tbl_ssbt_composite_info = function(con){dplyr::tbl(con, "ssbt_composite_info")}
 
 #' @export
-tbl_imp_benchmark = function(con){dplyr::tbl(con, "imp_benchmark")}
+tbl_ssbt_composite_info_account_info = function(con){dplyr::tbl(con, "ssbt_composite_info_account_info")}
 
 #' @export
-tbl_ticker = function(con){dplyr::tbl(con, "ticker")}
-
-#' @export
-tbl_city = function(con){dplyr::tbl(con, "city")}
-
-#' @export
-tbl_benchmark_info = function(con){dplyr::tbl(con, "benchmark_info")}
-
-#' @export
-tbl_benchmark_monthly = function(con){dplyr::tbl(con, "benchmark_monthly")}
-
-#' @export
-tbl_benchmark_daily = function(con){dplyr::tbl(con, "benchmark_daily")}
-
-#' @export
-tbl_pm_cashflow_quarterly = function(con){dplyr::tbl(con, "pm_cashflow_quarterly")}
-
-#' @export
-tbl_pm_gg_official_quarterly = function(con){dplyr::tbl(con, "pm_gp_official_quarterly")}
-
-#' @export
-tbl_book_of_record_monthly = function(con){dplyr::tbl(con, "book_of_record_monthly")}
-
-#' @export
-tbl_book_of_record_daily = function(con){dplyr::tbl(con, "book_of_record_daily")}
+tbl_sub_portfolio = function(con){dplyr::tbl(con, "sub_portfolio")}
--- R/get_account_info.R
+++ R/get_account_info.R
@@ -11,26 +11,21 @@ get_account_info = function(..., con = AZASRS_DATABASE_CONNECTION()){
   args = rlang::enexprs(...)
   tbl_name = 'account_info'
 
-
-  usr_tbl = dplyr::tbl(con, tbl_name)
-  dat = usr_tbl
-
-  dat = usr_tbl %>%
-    dplyr::left_join(tbl_category(con), by = c('category_id' = 'id')) %>%
-    dplyr::left_join(tbl_portfolio(con), by = c('portfolio_id' = 'id')) %>%
-    dplyr::left_join(tbl_asset_class(con), by = c('asset_class_id' = 'id')) %>%
-    dplyr::left_join(tbl_sub_portfolio(con), by = c('sub_portfolio_id' = 'id')) %>%
-    dplyr::left_join(tbl_previous_saa(con), by = c('previous_saa_id' = 'id')) %>%
-    dplyr::left_join(tbl_sponsor(con), by = c('sponsor_id' = 'id')) %>%
-    dplyr::left_join(tbl_saa_benchmark(con), by = c('saa_benchmark_id' = 'id')) %>%
-    dplyr::left_join(tbl_imp_benchmark(con), by = c('imp_benchmark_id' = 'id')) %>%
-    dplyr::left_join(tbl_ticker(con), by = c('ticker_id' = 'id')) %>%
-    dplyr::left_join(tbl_city(con), by = c('city_id' = 'id'))
-
+  dat = tbl_account_info(con)
 
   dat = dat %>%
-    dplyr::select(-benchmark_info_id, -asset_class_id, -portfolio_id, -sub_portfolio_id,
-                  -category_id, -previous_saa_id, -sponsor_id, -saa_benchmark_id, -imp_benchmark_id, -ticker_id, -city_id)
+    dplyr::left_join(tbl_category(con), by = c('category_id' = 'category_id')) %>%
+    dplyr::left_join(tbl_portfolio(con), by = c('portfolio_id' = 'portfolio_id')) %>%
+    dplyr::left_join(tbl_asset_class(con), by = c('asset_class_id' = 'asset_class_id')) %>%
+    dplyr::left_join(tbl_sub_portfolio(con), by = c('sub_portfolio_id' = 'sub_portfolio_id')) %>%
+    dplyr::left_join(tbl_previous_saa(con), by = c('previous_saa_id' = 'previous_saa_id')) %>%
+    dplyr::left_join(tbl_sponsor(con), by = c('sponsor_id' = 'sponsor_id')) %>%
+    dplyr::left_join(tbl_saa_benchmark(con), by = c('saa_benchmark_id' = 'saa_benchmark_id')) %>%
+    dplyr::left_join(tbl_imp_benchmark(con), by = c('imp_benchmark_id' = 'imp_benchmark_id'))
+
+  dat = dat %>%
+    dplyr::select(-asset_class_id, -portfolio_id, -sub_portfolio_id,
+                  -category_id, -previous_saa_id, -sponsor_id, -saa_benchmark_id, -imp_benchmark_id, -previous_saa_id)
 
   if(length(args) > 0){
     dat = dat %>%
