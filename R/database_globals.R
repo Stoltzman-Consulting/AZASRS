7f0ed847bbceb127939979e9682a9562c1f52c5a
Scott Stolzman <scotts@azasrs.gov>
2019-02-19 11:07:07 -0700

    Switch to P: drive

--- R/database_globals.R
+++ R/database_globals.R
@@ -1,8 +1,9 @@
 
 # Global variables
 #' @export
-AZASRS_DATABASE_LOCATION = "C:\\Users\\scotts\\Desktop\\2018 Database Project\\Database\\asrs_database.db"
-#AZASRS_DATABASE_LOCATION = "P:\\IMD\\2018 Database Project\\asrs_new.db"
+AZASRS_DATABASE_LOCATION = "P:\\IMD\\2018 Database Project\\asrs_new.db"
+# AZASRS_DATABASE_LOCATION = "C:\\Users\\scotts\\Desktop\\2018 Database Project\\Database\\asrs_database.db" ##For local development only
+
 
 #' @export
 AZASRS_DATABASE_DRIVER = RSQLite::SQLite()
