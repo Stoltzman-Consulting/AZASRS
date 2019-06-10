f59d36c1956c530d34c93cc1277d4c8029eb871c
Scott Stolzman <scotts@azasrs.gov>
2019-06-10 09:13:27 -0600

    Fixes

--- R/calc_irr.R
+++ R/calc_irr.R
@@ -8,28 +8,28 @@ calc_irr = function(cash_flow, dates){
 #' @export
 calc_irr_df_pm = function(nav_daily = get_pm_nav_daily(),
                           cf_daily = get_pm_cash_flow_daily(),
-                          group_var = pm_fund_id,
-                          date_cutoff = value_date()){
-  group_var = dplyr::enquo(group_var)
+                          date_cutoff = value_date(), ...){
+
+  group_vars = dplyr::enquos(...)
 
   nav_daily = nav_daily %>%
     dplyr::mutate(nav_cutoff = if_else(effective_date == valdate, nav, 0)) %>%
-    dplyr::select(!! group_var, effective_date, nav_cutoff) %>%
+    dplyr::select(!!! group_vars, effective_date, nav_cutoff) %>%
     dplyr::filter(nav_cutoff != 0) %>%
     dplyr::mutate(cash_flow = 0)
 
   nav_cf_daily = cf_daily %>%
-    dplyr::select(!! group_var, effective_date, cash_flow) %>%
+    dplyr::select(!!! group_vars, effective_date, cash_flow) %>%
     dplyr::mutate(nav_cutoff = 0) %>%
     dplyr::bind_rows(nav_daily) %>%
-    dplyr::group_by(!! group_var, effective_date) %>%
+    dplyr::group_by(!!! group_vars, effective_date) %>%
     dplyr::summarize(cash_flow = sum(cash_flow),
               nav_cutoff = sum(nav_cutoff)) %>%
     dplyr::mutate(cash_flow_cutoff = cash_flow + nav_cutoff)
 
   # Calculate IRR
   fund_irr = nav_cf_daily %>%
-    dplyr::group_by(!! group_var) %>%
+    dplyr::group_by(!!! group_vars) %>%
     dplyr::summarize(irr = calc_irr(cash_flow_cutoff, effective_date))
   return(fund_irr)
 }
