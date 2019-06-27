22399f187e60b4ddd8aaedb328e7a808c3bd5ac4
Scott Stolzman <scotts@azasrs.gov>
2019-06-27 08:19:13 -0600

    Suppress warnings for bind RCpp

--- tests/testthat/test-build_privm_metrics.R
+++ tests/testthat/test-build_privm_metrics.R
@@ -1,6 +1,8 @@
 context("test-build_privm_metrics")
 
 test_that("build_privm_metrics() returns matching tibble", {
-  privm_metrics = build_privm_metrics(pm_fund_portfolio, pm_fund_id)
+  suppressWarnings({
+    privm_metrics = build_privm_metrics(pm_fund_portfolio, pm_fund_id)
+  })
   expect_equal(privm_metrics, readRDS_test(privm_metrics))
 })
