61d031e6b31f034fbdaef73f277b02a2850aef76
Scott Stolzman <scotts@azasrs.gov>
2019-06-27 11:27:08 -0600

    Remove unused functions

--- tests/testthat/test-get_account_bor_monthly.R
+++
@@ -1,6 +0,0 @@
-context("test-get_account_bor_monthly")
-
-test_that("get_account_bor_monthly() returns matching tibble", {
-  account_bor_monthly = get_account_bor_monthly()
-  expect_equal(account_bor_monthly, readRDS_test(account_bor_monthly))
-})
