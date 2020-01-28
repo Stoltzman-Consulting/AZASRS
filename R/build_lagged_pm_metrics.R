9de759aa7041ee5a2e4516f5700d6f0d13126f67
Scott Stoltzman <scott@stoltzmanconsulting.com>
2020-01-27 21:15:55 -0700

    keeping a copy

--- R/build_lagged_pm_metrics.R
+++ R/build_lagged_pm_metrics.R
@@ -1,7 +1,7 @@
 #' @export
-build_lagged_pm_metrics = function(...,
+build_lagged_pm_metrics = function(pm_fund_portfolio, pm_fund_category, pm_fund_id,
                    con = AZASRS_DATABASE_CONNECTION(),
-                   start_date = '2016-12-31',
+                   start_date = '2019-06-30',
                    end_date = get_value_date(con = con),
                    time_delta = 'quarters',
                    n_qtrs = 4,
@@ -10,7 +10,7 @@ build_lagged_pm_metrics = function(...,
 
   # benchmark_lookup is a tibble to match for DVA & PME calcs
   # benchmark_lookup = tibble::tibble(pm_fund_portfolio = c("Credit", "PE",   "RE"), benchmark_id = c("ODCE",   "ODCE", "LSTA+250"))
-  test_exists = dplyr::enquos(...)
+  test_exists = dplyr::enquos(pm_fund_portfolio, pm_fund_category, pm_fund_id)
   if(is.null(test_exists$benchmark_lookup)){
     benchmark_lookup = default_benchmark_lookup
   }
@@ -20,16 +20,16 @@ build_lagged_pm_metrics = function(...,
     dplyr::summarize(effective_date = min(effective_date, na.rm = TRUE)) %>%
     dplyr::pull()
 
-  # bench_tbl = build_benchmark_fv_index_factor(...,
-  #                                             con = con,
-  #                                             start_date = min_nav_date,
-  #                                             value_date = end_date,
-  #                                             return_tibble = FALSE) %>%
-  #   dplyr::select(-benchmark_info_id, -index_factor) %>%
-  #   tibble::as_tibble()
-  #
-  # bench = benchmark_lookup %>%
-  #   dplyr::left_join(bench_tbl)
+  bench_tbl = build_benchmark_fv_index_factor(pm_fund_portfolio, pm_fund_category, pm_fund_id,
+                                              con = con,
+                                              start_date = min_nav_date,
+                                              value_date = end_date,
+                                              return_tibble = FALSE) %>%
+    dplyr::select(-benchmark_info_id, -index_factor) %>%
+    tibble::as_tibble()
+
+  bench = benchmark_lookup %>%
+    dplyr::left_join(bench_tbl)
 
   if(itd){
     dates_df = tibble::tibble(start_date = lubridate::as_date('1900-12-31'), end_date = lubridate::as_date(end_date), itd = TRUE)
@@ -39,124 +39,29 @@ build_lagged_pm_metrics = function(...,
       dplyr::mutate(itd = FALSE)
   }
 
-  dat = dates_df %>%
+  dat_prep = dates_df %>%
     dplyr::group_by(start_date, end_date) %>%
-    dplyr::mutate(nav_cash_flow = list(build_nav_cash_flow_combined(...,
+    dplyr::mutate(nav_cash_flow = list(build_nav_cash_flow_combined(pm_fund_portfolio, pm_fund_category, pm_fund_id,
                                                                     con = con,
                                                                     start_date = start_date,
                                                                     end_date = end_date,
                                                                     itd = itd,
-                                                                    return_tibble = TRUE)))
+                                                                    return_tibble = TRUE))) %>%
+    tidyr::unnest(cols = c(nav_cash_flow)) %>%
+    dplyr::group_by(pm_fund_portfolio, pm_fund_category, pm_fund_id, start_date, end_date, effective_date, itd) %>%
+    dplyr::summarize(nav_cash_flow = sum(nav_cf, na.rm = TRUE),
+                     contributions = sum(contributions, na.rm = TRUE),
+                     distributions = sum(distributions, na.rm = TRUE),
+                     nav = sum(nav)) %>%
+    dplyr::ungroup()
 
-  # if(itd){
-  #   dat_itd_prep = build_nav_cash_flow_combined(...,
-  #                                          con = con,
-  #                                          start_date = start_date,
-  #                                          end_date = end_date,
-  #                                          itd = itd,
-  #                                          return_tibble = TRUE) %>%
-  #     dplyr::left_join(bench)
-  #
-  #   dat_itd_prep_end = dat_itd_prep %>%
-  #     dplyr::group_by(...) %>%
-  #     dplyr::filter(effective_date == min(effective_date, na.rm = TRUE)) %>%
-  #     dplyr::select(..., last_index_value = index_value)
-  #
-  #   dat_itd = dat_itd_prep %>%
-  #     dplyr::left_join(dat_itd_prep_end) %>%
-  #     dplyr::mutate(index_factor = last_index_value / index_value) %>%
-  #     dplyr::group_by(...) %>%
-  #     dplyr::arrange(effective_date) %>%
-  #     dplyr::summarize(irr = calc_irr(cash_flow = nav_cf, dates = effective_date),
-  #                      tvpi = calc_tvpi(distributions, contributions, nav),
-  #                      dpi = calc_dpi(distributions, contributions),
-  #                      appreciation = calc_appreciation(contributions+distributions, nav),
-  #                      dva = calc_dva(contributions+distributions, index_factor),
-  #                      pme = calc_pme(distributions, contributions, nav, index_factor))
-  #
-  #   min_max_dates = get_pm_nav_daily(con = con, return_tibble = FALSE) %>%
-  #     dplyr::filter(effective_date %in% qtr_dates) %>%
-  #     dplyr::group_by(...) %>%
-  #     dplyr::summarize(start_date = min(effective_date, na.rm = TRUE), end_date = max(effective_date, na.rm = TRUE)) %>%
-  #     dplyr::ungroup() %>%
-  #     tibble::as_tibble()
-  #
-  #   dat_itd_final = dat_itd %>%
-  #     dplyr::left_join(min_max_dates) %>%
-  #     dplyr::left_join(benchmark_lookup) %>%
-  #     dplyr::mutate(itd = TRUE)
-  #
-  # }
-
-
-
-    tmp_irr_calc = function(df){
-      deselect_cols = paste(c('nav_cf', 'distributions', 'contributions', 'nav', 'index_value', 'index_factor'), collapse = "|")
-      deselect_cols_lower_level = paste(c('effective_date', 'nav_cf', 'distributions', 'contributions', 'nav', 'index_value', 'index_factor'), collapse = "|")
-
-      dat_prep = df %>%
-        dplyr::group_by_at(names(df)[-grep(deselect_cols, names(df))]) %>%
-        dplyr::summarize(nav_cf = sum(nav_cf, na.rm = TRUE),
-                         distributions = sum(distributions, na.rm = TRUE),
-                         contributions = sum(contributions, na.rm = TRUE),
-                         nav = sum(nav, na.rm = TRUE))
-
-      dat = dat_prep %>%
-        dplyr::group_by_at(names(dat_prep)[-grep(deselect_cols_lower_level, names(dat_prep))]) %>%
-        dplyr::arrange(effective_date) %>%
-        dplyr::summarize(irr = calc_irr(cash_flow = nav_cf, dates = effective_date),
-                         tvpi = calc_tvpi(distributions, contributions, nav),
-                         dpi = calc_dpi(distributions, contributions),
-                         appreciation = calc_appreciation(contributions+distributions, nav))
-
-      return(dat)
-    }
-
-    #   dat_with_bench = dat_prep %>%
-    #     dplyr::left_join(bench) # variable from parent function
-    #
-    #   dat_with_bench_end = dat_with_bench %>%
-    #     dplyr::group_by_at(names(dat_with_bench)[-grep(deselect_cols_lower_level, names(dat_with_bench))]) %>%
-    #     dplyr::filter(effective_date == max(effective_date, na.rom = TRUE)) %>%
-    #     dplyr::ungroup() %>%
-    #     dplyr::select(benchmark_id, index_value) %>%
-    #     dplyr::distinct(benchmark_id, index_value) %>%
-    #     dplyr::rename(last_index_value = index_value)
-    #
-    #   dat_with_bench_factor = dat_with_bench %>%
-    #     dplyr::left_join(dat_with_bench_end, by = 'benchmark_id') %>%
-    #     dplyr::mutate(index_factor = last_index_value / index_value)
-    #
-    #   dat = dat_with_bench_factor %>%
-    #     dplyr::group_by_at(names(dat_with_bench_factor)[-grep(deselect_cols_lower_level, names(dat_with_bench_factor))]) %>%
-    #     dplyr::arrange(effective_date) %>%
-    #     dplyr::summarize(irr = calc_irr(cash_flow = nav_cf, dates = effective_date),
-    #                      tvpi = calc_tvpi(distributions, contributions, nav),
-    #                      dpi = calc_dpi(distributions, contributions),
-    #                      appreciation = calc_appreciation(contributions+distributions, nav),
-    #                      dva = calc_dva(contributions+distributions, index_factor),
-    #                      pme = calc_pme(distributions, contributions, nav, index_factor))
-    #
-    #   return(dat)
-    # }
-
-    dat = dat %>%
-      dplyr::group_by(start_date, end_date) %>%
-      dplyr::mutate(irr = purrr::pmap(.l = list(nav_cash_flow),
-                                      .f = tmp_irr_calc)) %>%
-      dplyr::select(-nav_cash_flow) %>%
-      tidyr::unnest(cols = c(irr)) %>%
-      dplyr::mutate(lagged_period = round(as.integer(end_date - start_date)/365, 2),
-                    lagged_period = as.character(dplyr::if_else(lagged_period >= 1, round(lagged_period), lagged_period)),
-                    lagged_period = dplyr::if_else(lagged_period == '0.25','3 Months',
-                                                   dplyr::if_else(lagged_period == '0.50' | lagged_period == '0.5', '6 Months',
-                                                                  paste(lagged_period, 'Year'))),
-                    lagged_period = dplyr::if_else(itd == TRUE, 'ITD', lagged_period)) %>%
-      dplyr::ungroup()
-
-    # if(itd){
-    #   dat = dplyr::bind_rows(dat, dat_itd_final)
-    # }
+  dat = dat_prep %>%
+    dplyr::group_by(pm_fund_portfolio, pm_fund_category, pm_fund_id, start_date, end_date, itd) %>%
+    dplyr::arrange(effective_date) %>%
+    dplyr::summarize(irr = calc_irr(cash_flow = nav_cash_flow, dates = effective_date),
+                     tvpi = calc_tvpi(distributions, contributions, nav),
+                     dpi = calc_dpi(distributions, contributions),
+                     appreciation = calc_appreciation(contributions+distributions, nav))
 
   return(dat)
 }
--- R/build_nav_cash_flow_combined.R
+++ R/build_nav_cash_flow_combined.R
@@ -1,7 +1,7 @@
 #' Get and combine cash flows and nav according to irr.z calc
 #'
 #' @export
-build_nav_cash_flow_combined = function(pm_fund_portfolio, pm_fund_category, pm_fund_id,
+build_nav_cash_flow_combined = function(...,
                                         con = AZASRS_DATABASE_CONNECTION(),
                                         start_date = '2019-06-30',
                                         end_date = get_value_date(con = con),
@@ -11,14 +11,14 @@ build_nav_cash_flow_combined = function(pm_fund_portfolio, pm_fund_category, pm_
                                         return_tibble = FALSE){
 
   min_max_nav_dates = nav_daily %>%
-    dplyr::group_by(pm_fund_portfolio, pm_fund_category, pm_fund_id) %>%
+    dplyr::group_by(...) %>%
     dplyr::summarize(min_nav_date = min(effective_date, na.rm = TRUE),
                      max_nav_date = max(effective_date, na.rm = TRUE)) %>%
     dplyr::ungroup()
 
   max_non_zero_nav_dates = nav_daily %>%
     dplyr::filter(nav != 0) %>%
-    dplyr::group_by(pm_fund_portfolio, pm_fund_category, pm_fund_id) %>%
+    dplyr::group_by(...) %>%
     dplyr::summarize(max_non_zero_nav_date = max(effective_date, na.rm = TRUE)) %>%
     dplyr::ungroup()
 
@@ -27,10 +27,10 @@ build_nav_cash_flow_combined = function(pm_fund_portfolio, pm_fund_category, pm_
       dplyr::left_join(min_max_nav_dates) %>%
       dplyr::left_join(max_non_zero_nav_dates) %>%
       dplyr::filter(effective_date == min_nav_date | effective_date == max_non_zero_nav_date) %>%
-      dplyr::group_by(pm_fund_portfolio, pm_fund_category, pm_fund_id, effective_date) %>%
+      dplyr::group_by(..., effective_date) %>%
       dplyr::summarize(nav = sum(nav, na.rm = TRUE)) %>%
       dplyr::ungroup() %>%
-      dplyr::group_by(pm_fund_portfolio, pm_fund_category, pm_fund_id) %>%
+      dplyr::group_by(...) %>%
       dplyr::mutate(min_date = min(effective_date, na.rm = TRUE),
                     max_date = end_date) %>%
       dplyr::ungroup() %>%
@@ -42,11 +42,11 @@ build_nav_cash_flow_combined = function(pm_fund_portfolio, pm_fund_category, pm_
       # dplyr::mutate(nav = dplyr::if_else(is.na(min_nav_date), 0, nav)) %>%
       dplyr::filter(start_date >= min_nav_date, effective_date <= max_nav_date) %>%
       dplyr::filter(effective_date == start_date | effective_date == end_date) %>%
-      dplyr::group_by(pm_fund_portfolio, pm_fund_category, pm_fund_id) %>%
+      dplyr::group_by(...) %>%
       dplyr::mutate(n = dplyr::count()) %>%
       dplyr::filter(n == 2) %>%
       dplyr::select(-n) %>%
-      dplyr::group_by(pm_fund_portfolio, pm_fund_category, pm_fund_id, effective_date) %>%
+      dplyr::group_by(..., effective_date) %>%
       dplyr::summarize(nav = sum(nav, na.rm = TRUE)) %>%
       dplyr::mutate(nav = dplyr::if_else(effective_date == start_date, -1*nav, nav))
   }
@@ -58,7 +58,7 @@ build_nav_cash_flow_combined = function(pm_fund_portfolio, pm_fund_category, pm_
   cf =  cf_date_filter %>%
     dplyr::left_join(cash_flow_daily) %>%
     dplyr::filter(effective_date >= start_date, effective_date < end_date) %>%
-    dplyr::group_by(pm_fund_portfolio, pm_fund_category, pm_fund_id, effective_date) %>%
+    dplyr::group_by(..., effective_date) %>%
     dplyr::summarize(cash_flow = sum(cash_flow, na.rm = TRUE))
 
   # Prep for union
@@ -70,16 +70,16 @@ build_nav_cash_flow_combined = function(pm_fund_portfolio, pm_fund_category, pm_
   dat = dplyr::union_all(nav, cf) %>%
     dplyr::mutate(nav = dplyr::if_else(is.na(nav), 0, nav),
                   cash_flow = dplyr::if_else(is.na(cash_flow), 0, cash_flow)) %>%
-    dplyr::group_by(pm_fund_portfolio, pm_fund_category, pm_fund_id, effective_date) %>%
+    dplyr::group_by(..., effective_date) %>%
     dplyr::summarize(nav_cf = sum(nav, na.rm = TRUE) + sum(cash_flow, na.rm = TRUE)) %>%
-    dplyr::arrange(pm_fund_portfolio, pm_fund_category, pm_fund_id, effective_date) %>%
+    dplyr::arrange(..., effective_date) %>%
     dplyr::ungroup() %>%
     dplyr::mutate(distributions = dplyr::if_else(nav_cf > 0, nav_cf, 0),
            contributions = dplyr::if_else(nav_cf < 0, nav_cf, 0)) %>%
-    dplyr::group_by(pm_fund_portfolio, pm_fund_category, pm_fund_id) %>%
+    dplyr::group_by(...) %>%
     dplyr::mutate(nav = if_else(effective_date == min(effective_date, na.rm = TRUE) | effective_date == max(effective_date, na.rm = TRUE), nav_cf, 0)) %>%
     dplyr::ungroup() %>%
-    dplyr::group_by(pm_fund_portfolio, pm_fund_category, pm_fund_id, effective_date) %>%
+    dplyr::group_by(..., effective_date) %>%
     dplyr::summarize(nav_cf = sum(nav_cf, na.rm = TRUE),
                      distributions = sum(distributions, na.rm = TRUE),
                      contributions = sum(contributions, na.rm = TRUE),
