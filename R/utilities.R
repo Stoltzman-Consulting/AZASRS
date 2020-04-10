4ca6d4169cd25207a89839a07e0b8a0c0ca0cc83
Scott Stoltzman <scott@stoltzmanconsulting.com>
2020-04-10 11:01:00 -0600

    update %m-% dependency to explicit call

--- R/utilities.R
+++ R/utilities.R
@@ -24,7 +24,7 @@ test_is_not_rollup <- function(...) {
 calc_previous_year_qtr = function(end_date = get_value_date(), years = 0, qtrs = 0){
   end_date = lubridate::as_date(end_date)
   previous_year = end_date - lubridate::years(years)
-  previous_year_qtr = previous_year %m-% months(3*qtrs)
+  previous_year_qtr = lubridate::"%m-%"(previous_year, months(3*qtrs))
   previous_year_qtr = lubridate::round_date(previous_year_qtr, unit = 'quarter') - lubridate::days(1)
   return(previous_year_qtr)
 }
