7d8dd244df3d50bdb470aaa53f92a4d1ae2ac07d
Scott Stoltzman <scott@stoltzmanconsulting.com>
2020-04-10 11:27:58 -0600

    Remove poorly named test

--- tests/testthat/test-database_globals.R
+++ tests/testthat/test-database_globals.R
@@ -23,7 +23,14 @@ test_that("Next quarter is one after valdate", {
   expect_equal(next_quarter, "2019-12-31")
 })
 
-test_that("PM Fund Info has not changed", {
-  tbl_pm_fund_info = tbl_pm_fund_info() %>% tibble::as_tibble() %>% head(3)
-  expect_equal(tbl_pm_fund_info, readRDS_test(tbl_pm_fund_info) %>% head(3))
+test_that("PM Fund Info columns have not changed", {
+  tbl_pm_fund_info = tbl_pm_fund_info() %>% tibble::as_tibble()
+  colnames_tbl_pm_fund_info = colnames(tbl_pm_fund_info)
+  expected_colnames_tbl_pm_fund_info = c("pm_fund_info_id", "pm_fund_id", "pm_fund_description", "pm_fund_common_name",
+    "pm_fund_portfolio_id", "pm_fund_category_id", "pm_fund_category_description_id",
+    "pm_fund_sponsor_id", "pm_fund_city_id", "pm_fund_sector_id",
+    "vintage", "commit", "unfunded", "legacy", "specialist", "invest_end",
+    "term_end", "extension", "ext_time", "ext_used", "fee_cat", "consultant",
+    "adv_board", "obsvr", "fund_size_m", "closed")
+  expect_equal(colnames_tbl_pm_fund_info, expected_colnames_tbl_pm_fund_info)
 })
