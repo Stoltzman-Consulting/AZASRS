4287b64ece7fe10c17f369293fd54d67f918c9b6
Scott Stoltzman <scott@stoltzmanconsulting.com>
2020-04-13 07:51:25 -0600

    allow unauthenticated

--- .travis_build.sh
+++ .travis_build.sh
@@ -2,5 +2,5 @@
 set -ev
 sudo curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
 sudo apt-get update
-sudo ACCEPT_EULA=Y apt-get install msodbcsql17
+sudo ACCEPT_EULA=Y apt-get install --allow-unauthenticated msodbcsql17
 sudo apt-get install unixodbc-dev
