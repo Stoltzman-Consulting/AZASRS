05fc529f9a8f462ebea1ac3f5d21f98b4ec24dfc
Scott Stoltzman <scott@stoltzmanconsulting.com>
2020-04-19 11:09:43 -0600

    Add Azure functionality

--- R/get_raw_csv_from_azure.R
+++ R/get_raw_csv_from_azure.R
@@ -1,21 +1,19 @@
 #' Get CSV Data from Azure
 #'
 #' @description Gets RAW csv for IRR comparison from Azure
-#' @param data_directory is a relative path to specific folder where all CSV files must be loaded
+#' @param blob_directory is the folder within the blob
+#' @param filename is the filename within the blob's folder
+#' @param download_to_directory is the relative path where the data will be downloaded and stored to
+#' @param read_csv if the file is a CSV and you want it to be read, it will be returned as a tibble
 #' @export
-get_raw_csv_from_azure = function(data_directory = './reconciliation_data/'){
+get_raw_data_from_azure = function(blob_directory = 'source-data', filename = 'pm_fund_info.csv', download_to_directory = './', read_csv = TRUE){
 
-  cont = AzureStor::blob_container(Sys.getenv('ASRS_BLOB_PM_RECONCILIATION'), key = Sys.getenv('ASRS_BLOB_KEY'))
-  all_files = AzureStor::list_blobs(cont)
-  AzureStor::multidownload_blob(cont, '*.csv', data_directory, overwrite = TRUE)
+  cont = AzureStor::blob_container(paste0(Sys.getenv('ASRS_BLOB'), blob_directory),
+                                     key = Sys.getenv('ASRS_BLOB_KEY'))
+  download_data = AzureStor::download_blob(cont, src = filename, dest = paste0(download_to_directory, filename), overwrite = TRUE)
 
-  csv_irrs = tibble::tibble()
-  for(file in all_files$name){
-    tmp = readr::read_csv(paste0(data_directory, file)) %>%
-      dplyr::select(`Client Grouping 1`, `3 Month IRR`, `6 Month IRR`,
-             `1 Year IRR`, `3 Year IRR`, `5 Year IRR`, `7 Year IRR`, `10 Year IRR`, `ITD IRR`) %>%
-      tidyr::drop_na(`Client Grouping 1`)
-    csv_irrs = dplyr::bind_rows(csv_irrs, tmp)
-  return(csv_irrs)
+  if(read_csv){
+    return(download_date %>% readr::read_csv(download_data))
   }
+  print('Data has been downloaded successfully!')
 }
