a0677cc517d0061962465d401612a61ed0373df2
Scott Stoltzman <scott@stoltzmanconsulting.com>
2020-06-26 07:59:50 -0600

    Remove travis CI

--- .travis_build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -ev
-sudo curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
-sudo apt-get update
-sudo ACCEPT_EULA=Y apt-get install --allow-unauthenticated msodbcsql17
-sudo apt-get install --allow-unauthenticated unixodbc-dev
