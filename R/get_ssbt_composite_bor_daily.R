31dd58920989d40032c5f7310cec5015d560bfee
Kelly Loucks <kelly@stoltzmanconsulting.com>
2020-06-29 09:32:48 -0600

    Update get_ssbt_composite_bor_daily description

--- R/get_ssbt_composite_bor_daily.R
+++ R/get_ssbt_composite_bor_daily.R
@@ -1,16 +1,17 @@
 #' Get SSBT composite book of record daily
 #'
 #' @description Gets composite book of record daily
-#' @param ... filtering expressions. i.e. effective_date >= '2018-01-01'
+#' @param con is a database connection object from AZASRS::AZASRS_DATABASE_CONNECTION()
 #' @return Returns a tibble with all composite data but can be filtered.
+#' @param return_tibble is a boolean that determines whether or not a tibble is returned instead
 #' @examples
-#' get_ssbt_composite_bor_daily(effective_date >= "2018-01-01")
-#' # A tibble: 4,160 x 10
+#' get_ssbt_composite_bor_daily()
+#' # A tibble: 8,728 x 10
 #' # ssbt_composite_in~ effective_date   beginning_market~ ending_market_v~ net_cash_flow daily_return ssbt_composite_~
 #' #   <int>               <chr>              <dbl>            <dbl>         <dbl>        <dbl>           <chr>
-#' #    24               2018-06-30         1467349533.      1482968778.          0       0.0106          Comp1
-#' #    24               2018-07-02         1482974842.      1463381621.          0       0               Comp2
-#' # ... with 4,150 more rows, and 3 more variables: ssbt_composite_description <chr>,
+#' #    1               2018-06-30           39655974310.      39656200393.     0         0.0000057       ASRSA001
+#' #    1               2018-07-02           39633650853      39565538581     17053151    -0.00215        ASRSA001
+#' # … with 8,726 more rows, and 3 more variables: ssbt_composite_description <chr>,
 #' #  ssbt_composite_short_description <chr>, benchmark_info_id <int>
 #' @export
 get_ssbt_composite_bor_daily <- function(con = AZASRS_DATABASE_CONNECTION(), return_tibble = TRUE) {
