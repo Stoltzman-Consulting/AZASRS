fa449e538f6fc6c735622e0c15768f97e312f020
Scott Stoltzman <scott@stoltzmanconsulting.com>
2020-07-07 14:28:13 -0600

    Clean up docs

--- R/build_grouped_pm_cash_flow.R
+++ R/build_grouped_pm_cash_flow.R
@@ -4,29 +4,14 @@
 #' @param .data is from clean_nav_cf()
 #' @param ... aggregation choices from from pm_fund_info (i.e. pm_fund_portfolio, pm_fund_category, pm_fund_id)
 #' @param start_date is the start date of analysis
-#' @param end_date is the cutoff date of analysis
-#' @param itd is a boolean that determines whether itd (incpetion to date) is included
-#' @param cash_adjusted is a boolean that determines if cash is adjusted
+#' @param end_date is the cutoff date of analysis, typically a value date
+#' @param itd is a boolean that determines whether itd (incpetion to date) is included, overrides start date
+#' @param cash_adjusted is a boolean that determines if cash is adjusted NAV should be used
 #' @param nav_daily is the object of get_pm_nav_daily()
 #' @param cf_daily is the object of get_pm_cash_flow_daily()
 #' @param bench_daily is the object of get_benchmark_daily_index()
 #' @param bench_relationships is the object of get_benchmark_fund_relationship()
 #' @param pm_fund_info is the object of get_pm_fund_info()
-#' @return
-#' @examples
-#' # Example use case
-#' nav <- get_pm_nav_daily() %>% dplyr::filter(nav != 0)
-#' cf <- get_pm_cash_flow_daily() %>% dplyr::filter(cash_flow != 0)
-#' pm_fund_info <- get_pm_fund_info()
-#' start_date <- "2019-09-30"
-#' end_date <- "2019-12-31"
-#' itd <- FALSE
-#' cash_adjusted <- FALSE
-#' final_data <- build_grouped_pm_cash_flow(
-#'   start_date = start_date, end_date = end_date, itd = itd,
-#'   cash_adjusted = cash_adjusted, pm_fund_info = pm_fund_info,
-#'   pm_fund_portfolio, pm_fund_category_description
-#' )
 #' @export
 build_grouped_pm_cash_flow <- function(...,
                                        con = AZASRS_DATABASE_CONNECTION(),
--- R/build_grouped_pm_metrics.R
+++ R/build_grouped_pm_metrics.R
@@ -1,32 +1,16 @@
 #' Create rollup of IRR based off of dates
 #'
 #' @description Build IRR for any rollup and date range, including ITD and cash adjusted
-#' @param .data is from clean_nav_cf()
 #' @param ... aggregation choices from from pm_fund_info (i.e. pm_fund_portfolio, pm_fund_category, pm_fund_id)
 #' @param start_date is the start date of analysis
 #' @param end_date is the cutoff date of analysis
-#' @param itd is a boolean that determines whether itd (incpetion to date) is included
-#' @param cash_adjusted is a boolean that determines if cash is adjusted
+#' @param itd is a boolean that determines whether itd (incpetion to date) is included, overrides start_date
+#' @param cash_adjusted is a boolean that determines if NAV is cash adjusted
 #' @param nav_daily is the object of get_pm_nav_daily()
 #' @param cf_daily is the object of get_pm_cash_flow_daily()
 #' @param bench_daily is the object of get_benchmark_daily_index()
 #' @param bench_relationships is the object of get_benchmark_fund_relationship()
 #' @param pm_fund_info is the object of get_pm_fund_info()
-#' @return
-#' @examples
-#' # Example use case
-#' nav <- get_pm_nav_daily() %>% dplyr::filter(nav != 0)
-#' cf <- get_pm_cash_flow_daily() %>% dplyr::filter(cash_flow != 0)
-#' pm_fund_info <- get_pm_fund_info()
-#' start_date <- "2019-06-30"
-#' end_date <- "2019-09-30"
-#' itd <- FALSE
-#' cash_adjusted <- FALSE
-#' final_data <- build_grouped_irrs(
-#'   start_date = start_date, end_date = end_date, itd = itd,
-#'   cash_adjusted = cash_adjusted, pm_fund_info = pm_fund_info,
-#'   pm_fund_portfolio, pm_fund_category_description
-#' )
 #' @export
 build_grouped_pm_metrics <- function(...,
                                      con = AZASRS_DATABASE_CONNECTION(),
@@ -69,10 +53,10 @@ build_grouped_pm_metrics <- function(...,
 }
 
 
-#' Calculate the rollup IRR
+#' Calculate the rollup metrics
 #'
-#' @description Calculates the IRR from a list of "cash flow" from clean_nav_cf()
-#' @param .data is from clean_nav_cf()
+#' @description Calculates the major financial metrics
+#' @param .data is from build_grouped_pm_cash_flow()
 #' @param ... aggregation choices from from pm_fund_info (i.e. pm_fund_portfolio, pm_fund_category, pm_fund_id)
 calculate_grouped_pm_metrics <- function(.data, ...) {
   .data %>%
