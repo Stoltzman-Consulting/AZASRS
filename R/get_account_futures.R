73611f10a7992bf00d5f1e2cf94b0be05e3d6378
Scott Stoltzman <scott@stoltzmanconsulting.com>
2020-08-12 09:04:43 -0600

    explicit call to dplyr

--- R/get_account_futures.R
+++ R/get_account_futures.R
@@ -8,7 +8,8 @@
 get_account_futures <- function(con = AZASRS_DATABASE_CONNECTION(), return_tibble = TRUE) {
 
   dat <- tbl_account_futures(con = con) %>%
-    left_join(tbl_account_info(con = con) %>% select(account_info_id, account_id), by = 'account_info_id')
+    dplyr::left_join(tbl_account_info(con = con) %>%
+                       dplyr::select(account_info_id, account_id), by = 'account_info_id')
 
   if (return_tibble) {
     return(dat %>% tibble::as_tibble())
