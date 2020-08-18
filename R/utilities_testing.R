0166d7d7e70cc33cc46237c3ef1bcada8bc8c698
Kelly Loucks <kelly@stoltzmanconsulting.com>
2020-08-18 11:29:13 -0600

    Adding function description to check_database_tbl_matches

--- R/utilities_testing.R
+++ R/utilities_testing.R
@@ -1,4 +1,11 @@
 # PACKAGE TESTING HELPER FUNCTIONS
+#' @description Checks tbls in database globals to make sure formatting is consistent
+#' @param tbl_object the tbl object to be checked
+#' @param expected_names a character string with expected columns names
+#' @param types a character string with expected column data types
+#' @param greater_than_rows integer to test the tbl has rows greater than x
+#' @return error if any of the parameters do not match, no error otherwise
+
 check_database_tbl_matches = function(tbl_object, expected_names = c(), types = c(), greater_than_rows= n){
   a = tbl_object()
   testthat::expect_equal(
