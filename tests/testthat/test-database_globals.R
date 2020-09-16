768e5be86a9f5943e112dbb244f0287b7ab0abcc
Kelly Loucks <kelly@stoltzmanconsulting.com>
2020-09-16 16:57:31 -0600

    Fixing valdate test

--- tests/testthat/test-database_globals.R
+++ tests/testthat/test-database_globals.R
@@ -15,12 +15,12 @@ test_that("Database disconnect", {
 
 test_that("Proper value date (valdate)", {
   valdate <- get_value_date()
-  expect_equal(valdate, "2019-12-31")
+  expect_equal(valdate, "2020-03-31")
 })
 
 test_that("Next quarter is one after valdate", {
   next_quarter <- get_next_quarter()
-  expect_equal(next_quarter, "2020-03-31")
+  expect_equal(next_quarter, "2020-06-30")
 })
 
 test_that("PM Fund Info columns have not changed", {
