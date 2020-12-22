c65d6bdd85a1c8032979953f8941123ef8a61b80
Kelly Loucks <kelly@stoltzmanconsulting.com>
2020-12-21 17:04:00 -0700

    Add tests for get and build url functions

---
+++ tests/testthat/test-build_get_url.R
@@ -0,0 +1,9 @@
+context("test-build_get_url")
+
+test_that("build_get_url() returns character string of length 1", {
+  url_to_use <- build_get_url("pm_fund_info")
+
+  expect_equal(class(url_to_use), "character")
+
+  expect_equal(length(url_to_use), 1)
+})
