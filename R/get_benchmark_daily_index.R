0ec269a2c62e32786be340d0807d9ad46a6f0ff8
Kelly Loucks <kelly@stoltzmanconsulting.com>
2020-12-28 10:19:15 -0700

    Remove tibble option in functions

--- R/get_benchmark_daily_index.R
+++ R/get_benchmark_daily_index.R
@@ -1,18 +1,13 @@
 #' Get all benchmark_daily_index
 #'
 #' @description Gets benchmark index data by day
-#' @param return_tibble is a boolean that determines whether or not a tibble is returned instead
 #' @return Returns a tibble or data.frame object.
 #' @examples
 #' get_benchmark_daily_index(return_tibble = TRUE)
 #' @export
-get_benchmark_daily_index <- function(return_tibble = TRUE) {
+get_benchmark_daily_index <- function() {
 
   dat <- get_url_data("benchmark_daily_index")
 
-  if (return_tibble) {
-    return(dat %>% tibble::as_tibble())
-  } else {
-    return(dat)
-  }
+  return(dat)
 }
--- R/get_benchmark_info.R
+++ R/get_benchmark_info.R
@@ -1,18 +1,13 @@
 #' Get all benchmark_info
 #'
 #' @description A view to get all private market benchmark info
-#' @param return_tibble is a boolean that determines whether or not a tibble is returned instead
 #' @return Returns a tibble or data.frame result with all benchmark_info metadata.
 #' @examples
 #' get_benchmark_info()
 #' @export
-get_benchmark_info <- function(return_tibble = TRUE) {
+get_benchmark_info <- function() {
 
   dat <- get_url_data("benchmark_info")
 
-  if (return_tibble) {
-    return(dat %>% tibble::as_tibble())
-  } else {
-    return(dat)
-  }
+  return(dat)
 }
