d148b2b76c3df7a95e503de7ad7d8bc333d8111f
Kelly Loucks <kelly@stoltzmanconsulting.com>
2020-12-31 09:11:56 -0700

    Add pm fund info id

--- tests/testthat/test-get_pm_nav_daily.R
+++ tests/testthat/test-get_pm_nav_daily.R
@@ -3,7 +3,7 @@ context("test-get_pm_nav_daily")
 test_that("get_pm_nav_daily() matches test data", {
   pm_nav_daily <- get_pm_nav_daily()
   expected_names <- c(
-     "effective_date", "nav", "pm_fund_id", "pm_fund_description",
+    "pm_fund_info_id", "effective_date", "nav", "pm_fund_id", "pm_fund_description",
     "pm_fund_common_name", "vintage", "commit", "unfunded", "legacy",
     "specialist", "invest_end", "term_end", "extension", "ext_time",
     "ext_used", "fee_cat", "consultant", "adv_board", "obsvr", "fund_size_m",
@@ -12,6 +12,6 @@ test_that("get_pm_nav_daily() matches test data", {
   )
 
   expect_equal(colnames(pm_nav_daily), expected_names)
-  expect_equal(as.character(lapply(pm_nav_daily, class)), c("Date", "numeric", "character", "character", "character", "integer", "integer", "integer", "character", "character", "Date", "Date", "numeric", "numeric", "numeric", "character", "character", "logical", "logical", "numeric", "character", "character", "character", "character", "character", "character", "character"))
+  expect_equal(as.character(lapply(pm_nav_daily, class)), c("integer", "Date", "numeric", "character", "character", "character", "integer", "integer", "integer", "character", "character", "Date", "Date", "numeric", "numeric", "numeric", "character", "character", "logical", "logical", "numeric", "character", "character", "character", "character", "character", "character", "character"))
   expect_true(dplyr::count(pm_nav_daily) %>% dplyr::pull(n) > 1)
 })
