f0866cc289590b162f6a3614036c5b6f54377206
Kelly Loucks <kelly@stoltzmanconsulting.com>
2020-12-31 10:24:49 -0700

    Add documentation (benchmark info)

--- R/get_benchmark_info.R
+++ R/get_benchmark_info.R
@@ -1,4 +1,4 @@
-#' Get all benchmark_info
+#' Get all benchmark_info RAW
 #'
 #' @description A view to get all private market benchmark info
 #' @return Returns a tibble or data.frame result with all benchmark_info metadata.
@@ -12,7 +12,13 @@ get_benchmark_info_raw <- function() {
   return(dat)
 }
 
-
+#' Get all benchmark_info
+#'
+#' @description A view to get all private market benchmark info
+#' @return Returns a tibble or data.frame result with all benchmark_info metadata.
+#' @examples
+#' get_benchmark_info()
+#' @export
 
 get_benchmark_info <- function() {
 
