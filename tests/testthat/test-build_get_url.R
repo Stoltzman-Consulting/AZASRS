270231c046d1a8bc182fc4ec38be04e4738daa93
Kelly Loucks <kelly@stoltzmanconsulting.com>
2021-01-05 12:21:00 -0700

    Update build get url test (removing .Renviron objects)

--- tests/testthat/test-build_get_url.R
+++ tests/testthat/test-build_get_url.R
@@ -7,5 +7,16 @@ test_that("build_get_url() returns character string of length 1", {
 
   expect_equal(length(url_to_use), 1)
 
-  expect_error(build_get_url())
+  #testing that no input into function produces an error
+  expect_error(build_get_url(), "argument \"data_request\" is missing, with no default")
+
+  #check that removing MCR_DATA_RETRIEVAL_TOKEN throws error
+  Sys.setenv(AZASRS_DATA_RETRIEVAL_TOKEN = '')
+  expect_error(build_get_url("pm_fund_info"), "AZASRS_DATA_RETRIEVAL_TOKEN does not exist, check .Renviron file")
+
+  #check that removing MCR_BASE_URL throws error, (bringing back token)
+  Sys.setenv(AZASRS_DATA_RETRIEVAL_TOKEN = "TOKENHERE")
+  Sys.setenv(AZASRS_BASE_URL = '')
+  expect_error(build_get_url("pm_fund_info"), "AZASRS_BASE_URL does not exist, check .Renviron file")
+
 })
