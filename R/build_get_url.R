c503e34861f221e5d7ccd093d67a6f080b56e6dc
Kelly Loucks <kelly@stoltzmanconsulting.com>
2021-01-05 12:36:56 -0700

    Update url being built

--- R/build_get_url.R
+++ R/build_get_url.R
@@ -11,6 +11,6 @@ build_get_url = function(data_request){
   stopifnot("AZASRS_BASE_URL does not exist, check .Renviron file" = !identical(Sys.getenv("AZASRS_BASE_URL"), ''))
   stopifnot("AZASRS_DATA_RETRIEVAL_TOKEN does not exist, check .Renviron file" = !identical(Sys.getenv("AZASRS_DATA_RETRIEVAL_TOKEN"), ''))
 
-  return(paste0(Sys.getenv("AZASRS_BASE_URL"), data_request, "/", Sys.getenv("AZASRS_DATA_RETRIEVAL_TOKEN")))
+  return(paste0(Sys.getenv("AZASRS_BASE_URL"), "?token=", Sys.getenv("AZASRS_DATA_RETRIEVAL_TOKEN"), "&data=", data_request, ".csv"))
 
 }
