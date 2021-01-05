f8765d7a4262b99d675012d087b7788c1fdcdaad
Kelly Loucks <kelly@stoltzmanconsulting.com>
2021-01-05 12:32:48 -0700

    Use read_csv when grabbing data

--- R/get_url_data.R
+++ R/get_url_data.R
@@ -1,7 +1,7 @@
 #' Gets data from server
 #'
 #' @description Gets data from server - based on url from build_get_url()
-#' @param data_request is the dataframe desired (MUST BE IN QUOTES).. pm_fund_info, benchmark_index etc.
+#' @param data_request character dataframe desired (MUST BE IN QUOTES).. "pm_fund_info", "benchmark_index" etc.
 #' @examples
 #' get_url_data("benchmark_info")
 #' @return data.frame
@@ -12,6 +12,6 @@ get_url_data = function(data_request){
 
   DATA_URL <- build_get_url(data_request)
 
-  data = jsonlite::fromJSON(DATA_URL)
+  data = readr::read_csv(DATA_URL)
 }
 
