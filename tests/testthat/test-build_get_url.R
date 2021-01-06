308c63252dcdbea62ce31339e7502e01c892461f
Kelly Loucks <kelly@stoltzmanconsulting.com>
2021-01-06 11:29:40 -0700

    Update build url test

--- tests/testthat/test-build_get_url.R
+++ tests/testthat/test-build_get_url.R
@@ -1,6 +1,10 @@
 context("test-build_get_url")
 
 test_that("build_get_url() returns character string of length 1", {
+
+  ORIGINAL_AZASRS_BASE_URL = Sys.getenv('AZASRS_BASE_URL')
+  ORIGINAL_DATA_RETRIEVAL_TOKEN = Sys.getenv('AZASRS_DATA_RETRIEVAL_TOKEN')
+
   url_to_use <- build_get_url("pm_fund_info")
 
   expect_equal(class(url_to_use), "character")
@@ -15,8 +19,10 @@ test_that("build_get_url() returns character string of length 1", {
   expect_error(build_get_url("pm_fund_info"), "AZASRS_DATA_RETRIEVAL_TOKEN does not exist, check .Renviron file")
 
   #check that removing MCR_BASE_URL throws error, (bringing back token)
-  Sys.setenv(AZASRS_DATA_RETRIEVAL_TOKEN = "TOKENHERE")
+  Sys.setenv(AZASRS_DATA_RETRIEVAL_TOKEN = ORIGINAL_DATA_RETRIEVAL_TOKEN)
   Sys.setenv(AZASRS_BASE_URL = '')
   expect_error(build_get_url("pm_fund_info"), "AZASRS_BASE_URL does not exist, check .Renviron file")
+
+  Sys.setenv(AZASRS_BASE_URL = ORIGINAL_AZASRS_BASE_URL)
 
 })
