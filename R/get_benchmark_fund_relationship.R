1a3b310658e49663984d359f559ec8dbc93757c5
Kelly Loucks <kelly@stoltzmanconsulting.com>
2021-01-07 14:44:08 -0700

    Update benchmark relationship documentation and test

--- R/get_benchmark_fund_relationship.R
+++ R/get_benchmark_fund_relationship.R
@@ -1,20 +1,16 @@
 #' Get relationships between funds and benchmarks get_benchmark_fund_relationship
 #'
 #' @description Finds all data from benchmark_info_id matched with pm_fund_info_id
-#' @param con is a database connection object from AZASRS::AZASRS_DATABASE_CONNECTION()
-#' @param bench_type is the type of benchmark. The default is 'PVT' which is private.
-#' @param get_all_benchmark_types is a boolean that determines whether or not all bench types are returned
-#' @param return_tibble is a boolean that determines whether or not a tibble is returned instead
 #' @return Returns a tibble or database object
 #' @examples
-#' get_benchmark_fund_relationship(return_tibble = TRUE)
-#' # A tibble: 282 x 3
+#' get_benchmark_fund_relationship()
+#' # A tibble: 548 x 3
 #' # pm_fund_info_id benchmark_info_id benchmark_type
 #' # <int>                <int>               <chr>
 #' #  1                    53                 PVT
 #' #  2                    53                 PVT
 #' #  3                    53                 PVT
-#' # ... with 279 more rows
+#' # ... with 545 more rows
 #' @export
 ###ADD THIS ONE###
 get_benchmark_fund_relationship_raw <- function(){
@@ -23,17 +19,35 @@ get_benchmark_fund_relationship_raw <- function(){
   return(dat)
 }
 
+#' Get relationships between funds and benchmarks get_benchmark_fund_relationship
+#'
+#' @description Finds all data from benchmark_info_id matched with pm_fund_info_id
+#' @param con default TRUE
+#' @param bench_type is the type of benchmark. The default is 'PVT' which is private.
+#' @param get_all_benchmark_types is a boolean that determines whether or not all bench types are returned
+#' @param return_tibble default FALSE, is a boolean that determines whether or not a tibble is returned instead
+#' @return Returns a tibble or database object
+#' @examples
+#' get_benchmark_fund_relationship()
+#' # A tibble: 548 x 3
+#' # pm_fund_info_id benchmark_info_id benchmark_type
+#' # <int>                <int>               <chr>
+#' #  1                    53                 PVT
+#' #  2                    53                 PVT
+#' #  3                    53                 PVT
+#' # ... with 545 more rows
+#' @export
 
+get_benchmark_fund_relationship <- function(con = TRUE, bench_type = "PVT", get_all_benchmark_types = FALSE,
+                                            return_tible = FALSE) {
 
-get_benchmark_fund_relationship <- function(bench_type = "PVT", get_all_benchmark_types = FALSE) {
-  dat <- get_benchmark_fund_relationship_raw() %>%
-    dplyr::left_join(get_benchmark_info(), by = "benchmark_type_id") %>%
-    dplyr::select(pm_fund_info_id, benchmark_info_id, benchmark_type)
+  dat <- get_benchmark_fund_relationship_raw()
 
   if(!get_all_benchmark_types){
     dat = dat %>% dplyr::filter(benchmark_type == bench_type)
   }
     return(dat)
+
 }
 
 
